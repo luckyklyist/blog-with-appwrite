dc0824fd490648e8f75750116923172b9d5f3539
luckyklyist <puspaanupam@gmail.com>
2023-10-03 00:58:52 +0545

    appwrite auth part logic implemented

---
+++ src/appwrite/auth.ts
@@ -0,0 +1,68 @@
+import { Client, Account, ID } from "appwrite";
+import config from "../config/config";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteEndpoint)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async registerUser({
+    email,
+    password,
+    name,
+  }: {
+    email: string;
+    password: string;
+    name: string;
+  }) {
+    try {
+      let userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return userAccount;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async loginUser({ email, password }: { email: string; password: string }) {
+    try {
+      return this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUserSession() {
+    try {
+      return this.account.get();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async logoutUser() {
+    try {
+      return this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authservice = new AuthService();
+
+export default authservice;
