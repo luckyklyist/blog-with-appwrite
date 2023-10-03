59121b8c71ab8425083c3e587bfe06442d01e5a0
luckyklyist <puspaanupam@gmail.com>
2023-10-03 22:16:01 +0545

    added the dynamic navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,31 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const loginStatus = useSelector((state: RootState) => state.auth.status);
+  const navItems = [
+    {
+      name: "Blogs",
+      link: "/blogs",
+      status: loginStatus,
+    },
+    {
+      name: "Profile",
+      link: "/profile",
+      status: loginStatus,
+    },
+    {
+      name: "LogIn",
+      link: "/login",
+      status: !loginStatus,
+    },
+    {
+      name: "SignUp",
+      link: "/signup",
+      status: !loginStatus,
+    },
+  ];
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -10,12 +33,15 @@ const Header = () => {
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Blogs</a>
-          </li>
-          <li>
-            <a>Profile</a>
-          </li>
+          {navItems.map((item) => {
+            return (
+              item.status && (
+                <li key={item.name}>
+                  <a href={item.link}>{item.name}</a>
+                </li>
+              )
+            );
+          })}
           <li>{loginStatus ? <a>LogOut</a> : <a>LogIn</a>}</li>
         </ul>
       </div>
