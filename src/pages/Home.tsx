3d98ca587ebbe272f8c138ae833e23cdcc4b07e8
luckyklyist <puspaanupam@gmail.com>
2023-10-11 22:11:47 +0545

    added the header page with improved UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: "url(https://images3.alphacoders.com/133/1331008.png)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello, World</h1>
+          <p className="mb-5">
+            Appwrite Blog created by <a href="">Klyist</a>
+          </p>
+          <button className="btn btn-active btn-secondary">Read blogs</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
