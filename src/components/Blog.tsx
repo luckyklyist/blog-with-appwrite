442365f61e7642a946aa3c1cb8b6f0bb30298c59
luckyklyist <puspaanupam@gmail.com>
2023-10-11 21:38:26 +0545

    blog page added with UI tailwind

---
+++ src/components/Blog.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import service from "../appwrite/appwriteConfig";
+import { useParams } from "react-router-dom";
+
+interface Blog {
+  id: string;
+  title: string;
+  content: string;
+}
+
+const Blog = () => {
+  const { id } = useParams<{ id: string }>();
+  const [blog, setBlog] = React.useState<Blog>();
+  React.useEffect(() => {
+    async function getBlog() {
+      const documentList = await service.getPostId(String(id));
+      const blog = {
+        id: documentList["$id"],
+        title: documentList.title,
+        content: documentList.description,
+      };
+      setBlog(blog);
+    }
+    getBlog();
+  }, []);
+  return (
+    <div className="bg-gray-900 text-white min-h-screen">
+      <div className="max-w-3xl mx-auto py-12">
+        {blog ? (
+          <div>
+            <h1 className="text-4xl font-bold mb-8">{blog.title}</h1>
+            <p className="text-gray-400 mb-8">{blog.content}</p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
