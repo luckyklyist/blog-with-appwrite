4b02a63cb28b3dca27937b699a071115c2ec2c82
luckyklyist <puspaanupam@gmail.com>
2023-10-12 09:50:53 +0545

    added the rte forms

--- src/components/RTE.tsx
+++ src/components/RTE.tsx
@@ -2,22 +2,46 @@ import { Editor } from "@tinymce/tinymce-react";
 import React from "react";
 import { Controller } from "react-hook-form";
 
-const RTE = () => {
+interface RTEprops {
+  name: string;
+  control: any;
+  label?: string;
+  defaultValue?: string;
+}
+
+const RTE: React.FC<RTEprops> = ({
+  name,
+  control,
+  label,
+  defaultValue = "",
+}) => {
   return (
-    <Editor
-      initialValue="default value"
-      init={{
-        branding: false,
-        height: 500,
-        menubar: true,
-        plugins: [
-          "advlist autolink lists link image charmap print preview anchor",
-          "searchreplace visualblocks code fullscreen",
-          "insertdatetime media table paste code help wordcount",
-        ],
-        toolbar: "undo redo | formatselect | bold italic backcolor | ",
-      }}
-    />
+    <div>
+      {label && <label htmlFor={name}>{label}</label>}
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            init={{
+              height: 500,
+              menubar: true,
+              plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table paste code help wordcount",
+              ],
+              toolbar:
+                "undo redo | formatselect | bold italic backcolor | \
+            alignleft aligncenter alignright alignjustify | \
+            bullist numlist outdent indent | removeformat | help",
+            }}
+            onEditorChange={onChange}
+            value={defaultValue}
+          />
+        )}
+      />
+    </div>
   );
 };
 
